7f1c3e0b5595ad3eb021792866f64a5d45582eca
Caleb Adewole <caleberioluwa@gmail.com>
2022-04-20 09:55:52 +0100

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+	"time"
+)
+
+const version = "1.0.0"
+const cssVersion = "1"
+
+type config struct {
+	port int
+	env  string
+	api  string
+	db   struct {
+		dns string
+	}
+	stripe struct {
+		key    string
+		secret string
+	}
+}
+
+type application struct {
+	config        config
+	infoLog       *log.Logger
+	errorLog      *log.Logger
+	templateCache map[string]*template.Template
+	version       string
+}
+
+func (app *application) serve() error {
+
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		ReadTimeout:       10 * time.Second,
+		IdleTimeout:       30 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+
+	app.infoLog.Printf("starting http server on port %[1]d with url: http://localhost:%[1]d  ...", app.config.port)
+	return srv.ListenAndServe()
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 3000, "📌 app server port")
+	flag.StringVar(&cfg.env, "environment", "development", "📌 application runtime environment")
+	flag.StringVar(&cfg.api, "api", "http://localhost:3001", "📌 api endpoint entry for application")
+
+	cfg.stripe.key = os.Getenv("STRIPE_KEY")
+	cfg.stripe.secret = os.Getenv("STRIPE_SECRET")
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	tc := make(map[string]*template.Template)
+
+	app := &application{
+		config:        cfg,
+		templateCache: tc,
+		infoLog:       infoLog,
+		errorLog:      errorLog,
+		version:       "1.0.0",
+	}
+
+	if err := app.serve(); err != nil {
+		app.errorLog.Fatalln(err)
+	}
+}
---
+++ cmd/web/render.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"net/http"
+	"strings"
+	"text/template"
+)
+
+type templateData struct {
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float64
+	Data            map[string]interface{}
+	Flash           string
+	Warning         string
+	CSRFToken       string
+	IsAuthenticated bool
+	Error           string
+	CSSVersion      string
+	AppVersion      string
+	API             string
+}
+
+var functions = template.FuncMap{}
+
+//go:embed templates
+var templateFS embed.FS
+
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	return td
+}
+
+func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, page string, td *templateData, partials ...string) error {
+	var t *template.Template
+	var err error
+	templateToRender := fmt.Sprintf("templates/%s.gohtml", page)
+	_, exist := app.templateCache[templateToRender]
+	if app.config.env == "production" && exist {
+		t = app.templateCache[templateToRender]
+	} else {
+		t, err = app.parseTemplate(partials, page, templateToRender)
+		if err != nil {
+			app.errorLog.Println(err)
+			return err
+		}
+	}
+
+	if td == nil {
+		td = &templateData{}
+	}
+
+	td = app.addDefaultData(td, r)
+	err = t.Execute(w, td)
+	if err != nil {
+		app.errorLog.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (app *application) parseTemplate(partials []string, page, templateToRender string) (*template.Template, error) {
+	var t *template.Template
+	var err error
+
+	// building partials
+	if len(partials) > 0 {
+		for i, p := range partials {
+			partials[i] = fmt.Sprintf("templates/partials/%s.gohtml", p)
+		}
+	}
+	path := fmt.Sprintf("%s.gohtml", page)
+	if len(partials) > 0 {
+		t, err = template.New(path).Funcs(functions).ParseFS(
+			templateFS,
+			"templates/base.gohtml",
+			strings.Join(partials, ","),
+			templateToRender,
+		)
+	} else {
+		t, err = template.New(path).Funcs(functions).ParseFS(
+			templateFS,
+			"templates/base.gohtml",
+			templateToRender,
+		)
+	}
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return nil, err
+	}
+	app.templateCache[templateToRender] = t
+	return t, nil
+}

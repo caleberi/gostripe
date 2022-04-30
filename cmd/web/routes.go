9109bb91631705a270f4d319b5a756efaca3ce09
caleberi <caleberioluwa@gmail.com>
2022-04-30 19:32:31 +0100

    feat : add new features to web

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
+	mux.Post("/payment-succeeded", app.PaymentSucceeded)
 	return mux
 }

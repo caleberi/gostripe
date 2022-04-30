94cc496152f261b806c18c2adc961ebb260fda43
caleberi <caleberioluwa@gmail.com>
2022-04-30 19:30:56 +0100

    feat :  add request handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,40 @@ package main
 import "net/http"
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+	stringMap := make(map[string]string)
+	stringMap["publishable_key"] = app.config.stripe.key
+	if err := app.renderTemplate(w, r, "terminal", &templateData{
+		StringMap: stringMap,
+	}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
+
+func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	cardHolder := r.Form.Get("cardholder-name")
+	email := r.Form.Get("cardholder-email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+
+	data := map[string]interface{}{
+		"cardholder": cardHolder,
+		"email":      email,
+		"pi":         paymentIntent,
+		"pm":         paymentMethod,
+		"pa":         paymentAmount,
+		"pc":         paymentCurrency,
+	}
+
+	app.infoLog.Printf("data: %v", data)
+	if err := app.renderTemplate(w, r, "succeeded", &templateData{Data: data}); err != nil {
 		app.errorLog.Println(err)
 	}
 }

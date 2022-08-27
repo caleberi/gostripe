b6d0127e9af02461c063fb3e5611c299f85cfd8b
caleberi <caleberioluwa@gmail.com>
2022-08-27 21:18:42 +0100

    update card model

--- internal/cards/cards.go
+++ internal/cards/cards.go
@@ -2,12 +2,13 @@ package cards
 
 import (
 	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/customer"
 	"github.com/stripe/stripe-go/v72/paymentintent"
+	"github.com/stripe/stripe-go/v72/paymentmethod"
+	"github.com/stripe/stripe-go/v72/sub"
 )
 
 type Status int
-
-const ()
 
 type Card struct {
 	Secret   string
@@ -69,4 +70,71 @@ func cardErrorMessage(code stripe.ErrorCode) string {
 		msg = "Your card was declined"
 	}
 	return msg
+}
+
+// GetPaymentMethod retrieves the payment intent by payment id
+func (card *Card) GetPaymentMethod(s string) (*stripe.PaymentMethod, error) {
+	stripe.Key = card.Secret
+
+	pm, err := paymentmethod.Get(s, nil)
+
+	if err != nil {
+		return nil, err
+	}
+	return pm, nil
+}
+
+// RetrivePaymentIntent gets existing payment intent
+func (card *Card) RetrivePaymentIntent(id string) (*stripe.PaymentIntent, error) {
+	stripe.Key = card.Secret
+
+	pi, err := paymentintent.Get(id, nil)
+
+	if err != nil {
+		return nil, err
+	}
+	return pi, nil
+}
+
+func (card *Card) SubscribeToPlan(customer *stripe.Customer, plan, email, last4, cardType string) (*stripe.Subscription, error) {
+	stripeCustomerID := customer.ID
+	items := []*stripe.SubscriptionItemsParams{
+		{Plan: stripe.String(plan)},
+	}
+	params := &stripe.SubscriptionParams{
+		Customer: stripe.String(stripeCustomerID),
+		Items:    items,
+	}
+
+	params.AddMetadata("last_four", last4)
+	params.AddMetadata("card_type", cardType)
+	params.AddExpand("latest_invoice.payment_intent")
+	subscription, err := sub.New(params)
+	if err != nil {
+		return nil, err
+	}
+	return subscription, nil
+}
+
+func (card *Card) CreateCustomer(pm, email string) (*stripe.Customer, string, error) {
+	stripe.Key = card.Secret
+	customerParams := &stripe.CustomerParams{
+		PaymentMethod: stripe.String(pm),
+		Email:         stripe.String(email),
+		InvoiceSettings: &stripe.CustomerInvoiceSettingsParams{
+			DefaultPaymentMethod: stripe.String(pm),
+		},
+	}
+
+	customer, err := customer.New(customerParams)
+
+	if err != nil {
+		msg := ""
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			msg = cardErrorMessage(stripeErr.Code)
+		}
+		return nil, msg, err
+	}
+
+	return customer, "", nil
 }

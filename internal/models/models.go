8e46d41af4cdd46d19d4699fe91c0b6ba8c51d19
caleberi <caleberioluwa@gmail.com>
2022-08-27 21:19:11 +0100

    update widget

--- internal/models/models.go
+++ internal/models/models.go
@@ -31,9 +31,79 @@ type Widget struct {
 	Name           string    `json:"name"`
 	Description    string    `json:"description"`
 	InventoryLevel int       `json:"inventory_level"`
+	IsRecurring    bool      `json:"is_recurring"`
+	PlanID         string    `json:"plan_id"`
+	Image          string    `json:"image"`
 	Price          int       `json:"price"`
 	CreatedAt      time.Time `json:"-"`
 	UpdatedAt      time.Time `json:"-"`
+}
+
+// Order is the type for all order
+type Order struct {
+	ID            int       `json:"id"`
+	WidgetID      int       `json:"widget_id"`
+	TransactionID int       `json:"transaction_id"`
+	CustomerID    int       `json:"customer_id"`
+	StatusID      int       `json:"status_id"`
+	Quantity      int       `json:"quantity"`
+	Amount        int       `json:"amount"`
+	CreatedAt     time.Time `json:"-"`
+	UpdatedAt     time.Time `json:"-"`
+}
+
+// Status is the type for all order statues
+type Status struct {
+	ID        int       `json:"id"`
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+// TransactionStatus is the type for all transactional statues
+type TransactionStatus struct {
+	ID        int       `json:"id"`
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+// Transaction is the type for all transactions
+type Transaction struct {
+	ID                  int       `json:"id"`
+	Amount              int       `json:"amount"`
+	Currency            string    `json:"currency"`
+	LastFour            string    `json:"last_four"`
+	ExpiryMonth         int       `json:"expiry_month"`
+	ExpiryYear          int       `json:"expiry_year"`
+	PaymentMethod       string    `json:"payment_method"`
+	PaymenyIntent       string    `json:"payment_intent"`
+	BankReturnCode      string    `json:"bank_return_code"`
+	TransactionStatusID int       `json:"transaction_status_id"`
+	CreatedAt           time.Time `json:"-"`
+	UpdatedAt           time.Time `json:"-"`
+}
+
+// User is the type for all users
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	Lastname  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Image     string    `json:"image"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+// Customer is the type for all customer
+type Customer struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
 }
 
 func (m *DBModel) GetWidget(id int) (Widget, error) {
@@ -42,11 +112,132 @@ func (m *DBModel) GetWidget(id int) (Widget, error) {
 	defer cancel()
 
 	var widget Widget
-	row := m.DB.QueryRowContext(ctx, "SELECT id, name FROM widgets WHERE id = ?", id)
-	err := row.Scan(&widget.ID, &widget.Name)
+	row := m.DB.QueryRowContext(ctx, `
+		SELECT 
+			id, name, description, inventory_level, price, image,
+			plan_id, is_recurring, created_at, updated_at
+		FROM 
+			widgets 
+		WHERE id = ?`, id)
+	err := row.Scan(
+		&widget.ID,
+		&widget.Name,
+		&widget.Description,
+		&widget.InventoryLevel,
+		&widget.Price,
+		&widget.Image,
+		&widget.PlanID,
+		&widget.IsRecurring,
+		&widget.CreatedAt,
+		&widget.UpdatedAt,
+	)
 
 	if err != nil {
 		return widget, err
 	}
+
 	return widget, nil
+}
+
+// InsertTransaction inserts new transaction  and returns its id
+func (m *DBModel) InsertTransaction(txn Transaction) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+		INSERT INTO transactions
+			( amount, currency, last_four, bank_return_code, expiry_month, expiry_year, payment_intent, payment_method,
+				transaction_status_id, created_at, updated_at)
+		VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+	`
+	result, err := m.DB.ExecContext(ctx, query,
+		txn.Amount,
+		txn.Currency,
+		txn.LastFour,
+		txn.BankReturnCode,
+		txn.ExpiryMonth,
+		txn.ExpiryYear,
+		txn.PaymentMethod,
+		txn.PaymenyIntent,
+		txn.TransactionStatusID,
+		time.Now(),
+		time.Now(),
+	)
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+// InsertOrder inserts new order  and returns its id
+func (m *DBModel) InsertOrder(order Order) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+		INSERT INTO orders
+			( widget_id, transaction_id, status_id, quantity, customer_id,
+				amount, created_at, updated_at) 
+		VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)
+	`
+	result, err := m.DB.ExecContext(ctx, query,
+		order.WidgetID,
+		order.TransactionID,
+		order.StatusID,
+		order.Quantity,
+		order.CustomerID,
+		order.Amount,
+		time.Now(),
+		time.Now(),
+	)
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+func (m *DBModel) InsertCustomer(customer Customer) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+		INSERT INTO customers
+			( first_name, last_name, email, created_at, updated_at)
+		VALUES ( ?, ?, ?, ?, ?)
+	`
+	result, err := m.DB.ExecContext(ctx, query,
+		customer.FirstName,
+		customer.LastName,
+		customer.Email,
+		time.Now(),
+		time.Now(),
+	)
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }

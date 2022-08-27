ec76cc871f2181d1f290bfa97f3f8992db1d974d
caleberi <caleberioluwa@gmail.com>
2022-08-27 21:17:21 +0100

    add new routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,13 +9,20 @@ import (
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Use(SessionLoader)
 	mux.Use(middleware.Logger)
 	mux.Use(middleware.Recoverer)
+	mux.Get("/", app.RenderHomePage)
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
+	mux.Post("/virtual-terminal-payment-succeeded", app.VirtualTerminalPaymentSucceeded)
+	mux.Get("/virtual-terminal-receipt", app.VirtualTerminalReceipt)
 	mux.Post("/payment-succeeded", app.PaymentSucceeded)
-	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Get("/plans/bronze-plan", app.RenderBronzePlan)
+	mux.Get("/receipt", app.Receipt)
+	mux.Get("/receipt/bronze", app.BronzePlanReceipt)
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
-	mux.Get("/charge-once", app.ChargeOnce)
+	mux.Get("/widgets/{id}", app.ChargeOnce)
 
 	return mux
 }
